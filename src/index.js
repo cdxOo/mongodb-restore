2412bf32f7c5074965d973a20bf3b936e635e2d2
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-10-13 18:00:14 +0200

    added index entry for restore-buffer

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ module.exports = {
     dump: require('./restore-dump'),
     database: require('./restore-database'),
     collection: require('./restore-collection'),
+    buffer: require('./restore-buffer'),
 }
